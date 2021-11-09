839096a33b4a4812b63d4625cb055e518f2108f1
hugo23carvalho <45176065+hugo23carvalho@users.noreply.github.com>
2021-11-09 12:53:10 +0000

    Exercicio Unity

---
+++ PreExercicio5/Assets/Scripts/ConsoleMessages.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private EventMaster EM;
+
+    private void Awake()
+    {
+        EM = GetComponent<EventMaster>();
+    }
+
+    private void ShowConsoleMessages(char c) 
+    {
+        Debug.Log(c.ToString());
+    }
+
+    private void OnEnable()
+    {
+        EM.KeyPress += ShowConsoleMessages;
+    }
+
+    private void OnDisable()
+    {
+        EM.KeyPress += ShowConsoleMessages;
+    }
+}
+
+}
